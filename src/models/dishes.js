0c1c692ae89a0fd123f1758473689cd9fd9b4c05
noe lopez avila <noerr_@hotmail.com>
2020-06-08 17:51:14 -0500

    fixed/usecases_signup_login

--- src/models/dishes.js
+++ src/models/dishes.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const dishesSchema = new mongoose.Schema({
-  diets: {
-    name: {
-      type: String,
-      required: true
-    },
-    ingredients: {
-      type: String,
-      required: true
-    },
-    grams: {
-      type: Number,
-      required: true
-    },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
-    }
+
+  name: {
+    type: String,
+    required: true
+  },
+  ingredients: {
+    type: String,
+    required: true
+  },
+  grams: {
+    type: Number,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
   }
+
 })
 module.exports = mongoose.model('diets', dishesSchema)
