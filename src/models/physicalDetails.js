b55aaa870ef8d50694a433f16d30223d0f4f38a0
noemiAmaya <jazzamaya13@gmail.com>
2020-06-10 11:07:21 -0500

    changes model somatocarta

--- src/models/physicalDetails.js
+++ src/models/physicalDetails.js
@@ -38,13 +38,35 @@ const physicalDetailsSchema = new mongoose.Schema({
     ref: 'eatingPlan'
   }
 })
+
+const ectomorfo = {
+  type: 'Ectomorfo',
+  description: ' El cuerpo ectomorfo es delgado con extremidades largas' +
+    'y bajo peso normalmente.Suele tener un metabolismo acelerado.Este tipo de cuerpo no gana' +
+    'músculos con facilidad si bien deben hacer diversos tipos de ejercicios para estar más fuertes al tener aspecto' +
+    'frágil y hombros más bien pequeños.'
+}
+const mesomorfo = {
+  type: 'Mesomorfo',
+  description: 'Los mesomorfos son personas de contextura atlética que tienen sus músculos bastante bien desarrollados.' +
+    'Son muy fuertes y no deben de preocuparse por ganar o por perder peso pues su metabolismo funciona a la perfección.'
+}
+const endomorfo = {
+  type: 'Endomorfo',
+  description: ' Endomorfo: El cuerpo endomorfo, es un cuerpo redondeado y  con una tendencia natural a acumular grasa.' +
+    'Su metabolismo es más lento: es decir, el endomorfo quema menos calorías en estado de reposo.' +
+    'En general tiene menor tolerancia al carbohidrato.'
+}
+
 physicalDetailsSchema.virtual('physiognomy').get(function () {
   if (this.diameter >= 15 && this.diameter <= 17.5) {
-    return 'Ectomorfo'
-  } if (this.diameter >= 17.5 && this.diameter <= 20) {
-    return 'Mesomorfo'
-  } else (this.diameter >= 20)
-  return 'Endomorfo'
+    return (ectomorfo)
+  } else if (this.diameter >= 17.5 && this.diameter <= 20) {
+    return (mesomorfo)
+  } else if (this.diameter >= 20) {
+    return (endomorfo)
+  }
+  return 'no reconocido'
 })
 
 module.exports = mongoose.model('physicalDetails', physicalDetailsSchema)
