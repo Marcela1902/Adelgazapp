3d42a7efddc21527413d2f7c411bf6bbb4008d0a
noemiAmaya <jazzamaya13@gmail.com>
2020-06-12 17:11:20 -0500

    test fisionomia

--- src/usecases/physicalDetails.js
+++ src/usecases/physicalDetails.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+const EatingPlan= require ('../models/eatingPlan')
 const PhysicalDetails = require('../models/physicalDetails')
 const Users = require('../models/users')
 
@@ -6,14 +8,17 @@ function getAll () {
 }
 
 async function create (idUser, physicalDetailsData) {
-  const user = await Users.findById(idUser)
-  if (user.idTest) throw new Error('El test ya fue relizado')
+  console.log(idUser)
+  const users = Users.findById(idUser)
+  console.log(users)
+  const { idTest: test } = users
+  if (test) throw new Error('El test ya fue relizado')
   const physicalDetails = await PhysicalDetails.create(physicalDetailsData)
-  const { _id: idTest} = physicalDetails
+  const { _id: idTest, physiognomy } = physicalDetails
   await Users.findByIdAndUpdate(idUser, { idTest })
 
   /* const eatingsPlan = EatingPlan.find({ objective }) */
-  return physicalDetails.toObject({ virtuals: true })
+  return { physiognomy }
 
   /* const detail = await Users.findByIdAndUpdate(idUser, {
     $set: {
